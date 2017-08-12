f43a649ca3f50fd85cc9ac4c1da9a5c7734f966a
Matthew Snow <mattsnowis@yahoo.com>
2017-08-12 14:54:26 -0400

    Add home component

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import {Navigator} from 'react-native-deprecated-custom-components';
+
+import {HomeComponent} from './app/components/Home';
 
 export default class App extends React.Component {
+
+  renderScene(route, navigator) {
+    switch(route.id) {
+      case 'homecomponent':
+        return(<HomeComponent navigator={navigator}/>)
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Navigator
+        initialRoute={{id: 'homecomponent'}}
+        renderScene={this.renderScene}
+        configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom} />
     );
   }
 }
