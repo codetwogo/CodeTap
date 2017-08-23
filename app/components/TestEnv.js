a5884637611ed939fc2f47ceae1df618065abcb9
Matthew Snow <mattsnowis@yahoo.com>
2017-08-23 12:16:26 -0400

    fix: conflicts with index and testenv

--- app/components/TestEnv.js
+++ app/components/TestEnv.js
@@ -39,27 +39,30 @@ export default class TestEnvComponent extends Component {
   }
 
   componentDidMount() {
-    const resultArr = this.props.tests.map(test => {
-      return this.evaluateTest(test);
-    })
+    // const resultArr = this.props.tests.map(test => {
+    //   return this.evaluateTest(test);
+    // })
 
-    const isPassing = resultArr.reduce((a, b) => {
-      return a && b.pass
-    }, { "pass": true });
+    // const isPassing = resultArr.reduce((a, b) => {
+    //   console.log('bbbbbb', b.pass)
+    //   return a && b.pass
+    // }, {"pass": true});
 
-    this.setState({
-      tests: [...this.props.tests],
-      //userAnswer: this.props.userAnswer,
-      textStates: this.props.textStates,
-      isPassing: isPassing,
-      resultArr: resultArr
-    })
+    // this.setState({
+    //   tests: [...this.props.tests],
+    //   //userAnswer: this.props.userAnswer,
+    //   textStates: this.props.textStates,
+    //   isPassing: isPassing,
+    //   resultArr: resultArr
+    // })
+
   }
 
   navigateBack(msg) {
     if (msg === 'infinite loop') {
       // need to redirect to new CodeEnv and supply warning with hidden btn 
     }
+    console.log(this.props.textStates)
     this.props.navigator.push({
       id: 'back-code-env',
       userAnswer: this.props.userAnswer,
@@ -120,7 +123,6 @@ export default class TestEnvComponent extends Component {
 
     var callFunc;
 
-
     eval(`callFunc = ${this.state.userAnswer}`);
 
     const dataObj = {
@@ -138,20 +140,11 @@ export default class TestEnvComponent extends Component {
       if (i === this.state.tests.length - 1) dataObj.finished = true;
       console.log(this.state.tests[i]);
       dataObj.test = this.state.tests[i];
-      
-    const dataObj = {
-      func: callFunc.toString(),
-      args: this.state.tests[0].inputs
+
       this.webview.postMessage(JSON.stringify(dataObj));
     }
   }
 
-  }
-
-  getMessageFromWebView(data) {
-    const keys = Object.keys(data.nativeEvent);
-    console.log('data???', data.nativeEvent.data)
-  }
   getMessageFromWebView(data) {
     const msg = data.nativeEvent.data;
 
@@ -169,18 +162,8 @@ export default class TestEnvComponent extends Component {
 }
 
 
-    render() {
+render() {
 
-      return (
-
-        <WebView
-          style={{ marginTop: 20 }}
-          ref={webview => { this.webview = webview }}
-          source={require('../webviewScripts/load.html')}
-          onLoad={this.webViewLoaded.bind(this)}
-          onMessage={this.getMessageFromWebView.bind(this)} />
-      )
-    }
   return (
 
     <WebView
@@ -236,48 +219,82 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
     borderRadius: 10
   }
+});
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      padding: 10,
-      paddingTop: 12,
-      backgroundColor: '#333333'
-    },
-    topRowContainer: {
-      flex: 1
-    },
-    resultButton: {
-      flex: 1,
-      alignSelf: 'center',
-      backgroundColor: '#339933'
 
-    },
-    resultDetails: {
-      paddingTop: 3,
-      paddingBottom: 10
-    },
-    resultText: {
-      color: '#dd0000',
-      marginTop: 10,
-      marginBottom: 10,
-      fontWeight: 'bold',
-      fontSize: 22,
-      textAlign: 'center',
-      paddingTop: 35,
-      paddingBottom: 35,
-      borderColor: '#dd0000',
-      borderWidth: 2.5,
-      backgroundColor: '#bbb'
-    },
-    inputOutput: {
-      flex: 1,
-      backgroundColor: '#555555',
-      borderWidth: 1,
-      borderColor: '#555555',
-      padding: 15,
-      paddingBottom: 15,
-      borderRadius: 10
-    }
-  })
+//       <Container >
+//         <HeaderComponent navigator={this.props.navigator} style={styles.item}/>
+//         <Content style={styles.container}>
+//           <View style={styles.topRowContainer}>
+//             <View style={{
+//               flex: 1,
+//               flexDirection: 'row',
+//               alignItems: 'center',
+//               justifyContent: 'center'
+//             }}>
+
+//               <Text style={{
+//                 flex: 1,
+//                 textAlign: 'center',
+//                 height: 25,
+//                 paddingTop: 5,
+//                 fontSize: 20,
+//                 fontWeight: 'bold',
+//                 color: '#aaaaaa'
+//               }}>Result</Text>
+
+//             </View>
+//           </View>
+//           {(this.state.isPassing)
+//             ? <View>
+//                 <Text style={StyleSheet.flatten([
+//                   styles.resultText, {
+//                     color: '#339933',
+//                     borderColor: '#339933'
+//                   }
+//                 ])}>
+//                   Congratulations,{`\n`}
+//                   you've passed all the tests!!!!</Text>
+//               </View>
+//             : <View>
+//               <Text style={styles.resultText}>
+//                 Sadly, you've FAILED {`\n`}one or more tests!!!!</Text>
+//             </View>
+// }
+//           {this.state.resultArr.map((result, idx) => {
+//             return (
+//               <View style={styles.resultDetails} key={idx}>
+//                 {result.error
+//                   ? `Error received: ${result.error}`
+//                   : <View style={styles.inputOutput}>
+//                     <Text style={{color: '#aaa'}}>Inputs: [{result.inputs}]
+//                     </Text>
+//                     <Text style={{color: '#66aa55'}}>Expected Output: {result.output}
+//                     </Text>
+//                     <Text style={{color: '#cc7777'}}>
+//                       Actual Output : {result.result}
+//                     </Text>
+//                   </View>}
+//               </View>
+//             )
+//           })
+// }
+
+//           {(this.state.isPassing)
+//             ? <View>
+//                 <Button style={styles.resultButton} onPress={this.navigateToAllQuestions}>
+//                   <Text>Go back to all questions</Text>
+//                 </Button>
+//               </View>
+//             : <View>
+//               <Button danger style={StyleSheet.flatten([
+//                 styles.resultButton, {
+//                   backgroundColor: '#dd0000'
+//                 }
+//               ])} onPress={this.navigateBack}>
+//                 <Text style={{color: '#aaa'}}>Try again</Text>
+//               </Button>
+//             </View>
+// }
+//         </Content>
+//       </Container>
