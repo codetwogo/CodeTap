931631ceb7afff464fa3b988def3052d7b6010f5
Alexander Zoitos <azoitos1@gmail.com>
2017-09-04 13:45:52 -0400

    Update: Clean Up Code for Editor Folder

--- app/components/Editor/SwitchView.js
+++ app/components/Editor/SwitchView.js
@@ -1,55 +1,101 @@
 import React from 'react';
-import {Text, Switch, StyleSheet} from 'react-native';
-import { Container, Header, DeckSwiper, Card, CardItem, Thumbnail, Left, Body, Icon, Button, Input, View } from 'native-base';
-
+import { Text, Switch, StyleSheet } from 'react-native';
+import { Container, Button, View } from 'native-base';
 
 import ClipButtons from '../ClipButtons';
 
-export default ({switchVal, onSwitchChange, onQuestionSwitchChange, switchQuestion, description, edit, showQuestion, undo}) => {
-    return (
-        <Container style={styles.container}>
-          <View  style={styles.midRow}>
-            <View style={{flex: 1}}>
-              <Button
-                onPress={ ()=> undo() }
-                style={styles.undoButton}>
-                <Text style={{color: '#444444'}}>Undo</Text>
-              </Button>
-            </View>
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
-              <Text style={{color:'#777777', fontSize: 13, width: 66, textAlign: 'right', paddingRight: 5}}>Smart Keyboard</Text>
-              <Switch
-                value={switchVal}
-                onValueChange={(value) => {onSwitchChange(value);}} />
-            </View>
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
-              <Text style={{color:'#777777', fontSize: 13, width: 66, textAlign: 'right', paddingRight: 5}}>Detailed Question</Text>
-              <Switch
-                value={showQuestion}
-                onValueChange={(value) => {onQuestionSwitchChange(value);}} />
-            </View>
-          </View>
-          <View style={{flex:1}}>
-            <View style={{ flex: 1, height:75,  justifyContent: 'flex-start' }}>
-              {
-                showQuestion ?
-                <Text style={{flex: 2, color: '#779973', lineHeight: 22,  padding: 5,  alignItems: 'stretch' }}>
-                  {description}
-                </Text>
-                : null
-              }
-              {
-                switchVal ?
-                <View style={{flex: 1, minHeight: 100}}>
-                  <ClipButtons edit={edit}  />
-                </View>
-                :
-                null
-              }
-            </View>
-          </View>
-        </Container>
-    );
+export default ({ switchVal, onSwitchChange, onQuestionSwitchChange, switchQuestion, description, edit, showQuestion, undo }) => {
+  return (
+    <Container style={styles.container}>
+      <View style={styles.midRow}>
+        <View style={{ flex: 1 }}>
+          <Button
+            onPress={() => undo()}
+            style={styles.undoButton}>
+            <Text style={{ color: '#444444' }}>Undo</Text>
+          </Button>
+        </View>
+        <View style={
+          {
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'flex-end'
+          }
+        }>
+          <Text style={
+            {
+              color: '#777777',
+              fontSize: 13,
+              width: 66,
+              textAlign: 'right',
+              paddingRight: 5
+            }
+          }>Smart Keyboard</Text>
+          <Switch
+            value={switchVal}
+            onValueChange={(value) => { onSwitchChange(value); }} />
+        </View>
+        <View style={
+          {
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'flex-end'
+          }
+        }>
+          <Text style={
+            {
+              color: '#777777',
+              fontSize: 13,
+              width: 66,
+              textAlign: 'right',
+              paddingRight: 5
+            }
+          }>Detailed Question</Text>
+          <Switch
+            value={showQuestion}
+            onValueChange={(value) => { onQuestionSwitchChange(value); }} />
+        </View>
+      </View>
+      <View style={{ flex: 1 }}>
+        <View style={
+          {
+            flex: 1,
+            height: 75,
+            justifyContent: 'flex-start'
+          }
+        }>
+          {
+            showQuestion ?
+              <Text style={
+                {
+                  flex: 2,
+                  color: '#779973',
+                  lineHeight: 22,
+                  padding: 5,
+                  alignItems: 'stretch'
+                }
+              }>
+                {description}
+              </Text>
+              : null
+          }
+          {
+            switchVal ?
+              <View style={
+                {
+                  flex: 1,
+                  minHeight: 100
+                }
+              }>
+                <ClipButtons edit={edit} />
+              </View>
+              :
+              null
+          }
+        </View>
+      </View>
+    </Container>
+  );
 };
 
 const styles = StyleSheet.create({
--- app/components/Editor/TextIDE.js
+++ app/components/Editor/TextIDE.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TextInput, StyleSheet } from 'react-native';
 
 export default ({ textEnvChange, textValue, textFocus, onSelectionChange, cursorBlur, switchVal }) => {
